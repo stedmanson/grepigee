94b9cd312a422e6fc28d856af9d4ba7f5b0a8b08
Scott Tedmanson <stedmanson@woolworths.com.au>
2024-03-24 09:47:10 +1100

    Remove default comments

--- cmd/cli/find.go
+++ cmd/cli/find.go
@@ -38,16 +38,7 @@ var findCmd = &cobra.Command{
 func init() {
 	rootCmd.AddCommand(findCmd)
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// findCmd.PersistentFlags().String("foo", "", "A help for foo")
 	findCmd.Flags().StringVarP(&environment, "env", "e", "", "Specify the environment to search in")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// findCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
 func processSharedFlows(environment string) {
@@ -80,6 +71,7 @@ func processProxies(environment string) {
 	}
 
 	deployedProxyList := apigee.GetProxyDeployments(proxyList, environment)
+
 	apigee.DownloadProxyRevision(deployedProxyList, environment)
 
 	foundProxyItems, err := searcher.SearchInDirectory(environment+"/proxies", "(?i)api-ecs")
